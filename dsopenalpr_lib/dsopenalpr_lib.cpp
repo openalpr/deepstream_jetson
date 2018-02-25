02398aa57cd41ca2859fc12e3992ad2b58003e6e
Matt Hill <matthill@openalpr.com>
2018-02-25 11:24:55 -0500

    Added partial OpenALPR lib implementation

---
+++ dsopenalpr_lib/dsopenalpr_lib.cpp
@@ -0,0 +1,139 @@
+/**
+ * Copyright (c) 2017, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * NVIDIA Corporation and its licensors retain all intellectual property
+ * and proprietary rights in and to this software, related documentation
+ * and any modifications thereto.  Any use, reproduction, disclosure or
+ * distribution of this software and related documentation without an express
+ * license agreement from NVIDIA Corporation is strictly prohibited.
+ *
+ */
+
+#include "dsopenalpr_lib.h"
+
+#include <glib.h>
+#include <stdio.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "openalprimpl.h"
+
+#define DEBUG_PRINTF(...) printf(__VA_ARGS__)
+//#define DEBUG_PRINTF(f_, ...) 
+
+
+struct DsOpenalprCtx
+{
+  DsOpenalprInitParams init_params;
+  OpenAlprImpl* impl;
+};
+
+DsOpenalprCtx *
+DsOpenalprCtx_Init (DsOpenalprInitParams * init_params)
+{
+  DEBUG_PRINTF("DsOpenalprCtx_Init\n");
+  
+  DsOpenalprCtx *ctx = (DsOpenalprCtx *) g_malloc (sizeof (DsOpenalprCtx));
+  ctx->init_params = *init_params;
+  ctx->impl = new OpenAlprImpl(init_params->country, init_params->config_file, 
+                               init_params->runtime_dir, init_params->license_key);
+  
+  if (init_params->group_plates_across_frames)
+  {
+    if (init_params->recognize_vehicles)
+      ctx->impl->enable_vehicle_classifier();
+  }
+  else
+  {
+    ctx->impl->disable_grouping();
+  }
+  
+  return ctx;
+}
+
+// Feeds input image data into OpenALPR stream buffer
+void
+DsOpenalpr_QueueInput (DsOpenalprCtx * ctx, unsigned char *data)
+{
+  DEBUG_PRINTF("DsOpenalpr_QueueInput\n");
+  // Queue input. Start processing.
+  // Here we just add the input pointer to a queue
+  int arraySize = ctx->init_params.processing_width * ctx->init_params.processing_height * ctx->init_params.processing_channels;
+  std::cout << "1" << std::endl;
+  alprcvgpu::Mat imgData = alprcvgpu::Mat(arraySize, 1, CV_8U, data);
+  std::cout << "2" << std::endl;
+  alprcvgpu::Mat img = imgData.reshape(ctx->init_params.processing_channels, ctx->init_params.processing_height);
+  std::cout << "Image size; " << img.cols << "x" << img.rows << std::endl;
+  alprcvgpu::imwrite("/tmp/test.jpg", img);
+  
+  ctx->impl->push_cv_mat(data, ctx->init_params.processing_width, 
+                         ctx->init_params.processing_height, ctx->init_params.processing_channels);
+
+}
+
+// Processes all frames in buffer.  
+// Returns individual results (plate results found in each frame)
+// and OpenALPR "group" results (a single plate result per vehicle.
+// Group results are delayed until after the vehicle has passed, individual results are instant.
+DsOpenalprOutput *
+DsOpenalpr_DequeueOutput (DsOpenalprCtx * ctx)
+{
+  DEBUG_PRINTF("DsOpenalpr_DequeueOutput\n");
+  
+  // Process the batch
+  std::vector<alpr::AlprResults> results = ctx->impl->process_batch();
+  DEBUG_PRINTF("DsOpenalpr_DequeueOutput processed batch\n");
+  std::vector<alpr::AlprGroupResult> group_results = ctx->impl->pop_groups();
+  DEBUG_PRINTF("DsOpenalpr_DequeueOutput popped groups\n");
+  
+  DsOpenalprOutput *out = new (DsOpenalprOutput);
+  
+  out->frame_results = results;
+  out->group_results = group_results;
+
+  DEBUG_PRINTF("DsOpenalpr_DequeueOutput returning output\n");
+  // Processing finished. Dequeue
+
+
+  // Fill output structure using processed output
+  // Here, we fake some detected objects and labels
+//  if (ctx->init_params.full_frame) {
+//    out->num_objects = 2;
+//    out->object[0] = (DsOpenalpr_Object) {
+//    ctx->init_params.processing_width / 8,
+//          ctx->init_params.processing_height / 8,
+//          ctx->init_params.processing_width / 8,
+//          ctx->init_params.processing_height / 8, "Obj0"};
+//    out->object[1] = (DsOpenalpr_Object) {
+//    ctx->init_params.processing_width / 2,
+//          ctx->init_params.processing_height / 2,
+//          ctx->init_params.processing_width / 8,
+//          ctx->init_params.processing_height / 8, "Obj1"};
+//  } else {
+//    out->num_objects = 1;
+//    out->object[0] = (DsOpenalpr_Object) {
+//    ctx->init_params.processing_width / 8,
+//          ctx->init_params.processing_height / 8,
+//          ctx->init_params.processing_width / 8,
+//          ctx->init_params.processing_height / 8, ""};
+//    g_snprintf (out->object[0].label, 64, "Obj_label");
+//  }
+
+  return out;
+}
+
+void
+DsOpenalprCtx_Deinit (DsOpenalprCtx * ctx)
+{
+  DEBUG_PRINTF("DsOpenalprCtx_Deinit\n");
+
+  delete ctx->impl;
+}
+
+
+void
+DsOpenalprCtx_ReleaseOutput (DsOpenalprOutput *ds_openalpr_output)
+{
+  DEBUG_PRINTF("DsOpenalprCtx_Deinit\n");
+
+  delete ds_openalpr_output;
+}
--- dsopenalpr_lib/dsopenalpr_lib.h
+++ dsopenalpr_lib/dsopenalpr_lib.h
@@ -12,32 +12,63 @@
 #ifndef __DSOPENALPR_LIB__
 #define __DSOPENALPR_LIB__
 
+#include <alprstream.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 typedef struct DsOpenalprCtx DsOpenalprCtx;
 
+// This value should be greater than the number of frames that DeepStream
+// could possibly send OpenALPR before triggering a processing event.
+// If it's too large, it will use more memory, but otherwise won't affect efficiency
+// if the value is too small, and DeepStream pushes a frame while we're full, a random frame is dropped from the queue
+// When processing happens, the queue is cleared.
+const int MAX_FRAME_QUEUE_SIZE = 30;
+
 typedef struct
 {
+  /// Width of the images being provided by DeepStream
   int processing_width;
+  
+  /// height of the images being provided by DeepStream
   int processing_height;
-  int full_frame;
+
+  // Number of channels in Mat provided by DeepStream
+  int processing_channels;
+  
+  /// Country controls the OpenALPR database loaded.  For example, USA/Mexico/Canada would be "us"
+  /// European plates would be "eu", Australia "au", etc.  A full list can be found in /usr/share/openalpr/runtime_data/config/
+  const char* country;
+  
+  /// The full path to the OpenALPR config file.  Default is /etc/openalpr/openalpr.conf if left blank.
+  const char* config_file;
+  
+  /// The full path to the OpenALPR runtime_data dir.  Default is /usr/share/openalpr/runtime_data if left blank.
+  const char* runtime_dir;
+  
+  /// The commercial license key to run.  If left blank it will use the value in /etc/openalpr/license.conf
+  const char* license_key;
+  
+  /// Perform vehicle recognition (once per vehicle not once per plates).  Can be disabled if not used to save memory
+  /// Requires group_plates_across_frames to be enabled in order to work
+  int recognize_vehicles;
+  
+  /// Group plates across frames in addition to returning individual results per frame.  Can be disabled to save memory.
+  int group_plates_across_frames;
 } DsOpenalprInitParams;
 
-typedef struct
-{
-  int left;
-  int top;
-  int width;
-  int height;
-  char label[64];
-} DsOpenalpr_Object;
 
 typedef struct
 {
-  int num_objects;
-  DsOpenalpr_Object object[4];
+  /// Individual frame result (one for each image frame processed)
+  std::vector<alpr::AlprResults> frame_results;
+  
+  /// Vehicle results are sent after the plate is no longer seen
+  /// one per vehicle (delayed), unrelated to the number of images
+  std::vector<alpr::AlprGroupResult> group_results;
+
 } DsOpenalprOutput;
 
 // Initialize library context
@@ -51,6 +82,9 @@ DsOpenalprOutput *DsOpenalpr_DequeueOutput (DsOpenalprCtx *ctx);
 
 // Deinitialize library context
 void DsOpenalprCtx_Deinit (DsOpenalprCtx *ctx);
+
+/// Free the memory used by the output object
+void DsOpenalprCtx_ReleaseOutput (DsOpenalprOutput *ds_openalpr_output);
 
 #ifdef __cplusplus
 }
